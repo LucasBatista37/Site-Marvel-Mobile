d110ef2513e2ca998307f42a1d88bbfdd136c6b5
LucasBatista37 <lucas.batista9734@gmail.com>
2023-09-09 03:17:41 +0900

    menu

--- www/interagir.js
+++ www/interagir.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.querySelector('.menu-button');
     const menu = document.querySelector('.menu');
-    
+    const fechar = document.querySelector('.fechar'); 
+
     menuButton.addEventListener('click', function () {
-        if (menu.style.height === "100%") {
-            menu.style.height = "0";
+        if (menu.style.left === "0") {
+            menu.style.left = "-100%";
             menu.style.opacity = "0";
             menu.style.visibility = "hidden";
         } else {
-            menu.style.height = "100%";
+            menu.style.left = "0";
             menu.style.opacity = "1";
             menu.style.visibility = "visible";
         }
-    });    
-});
 
+    document.body.style.overflow = 'hidden';
+    });
+
+    fechar.addEventListener('click', function () { 
+        menu.style.left = "-100%";
+        menu.style.opacity = "0";
+        menu.style.visibility = "hidden";
+
+    document.body.style.overflow = 'auto';        
+    });
+});
