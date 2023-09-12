a16d0c26182b3019624eb30bd47f8b9c6e12db9b
LucasBatista37 <lucas.batista9734@gmail.com>
2023-09-12 12:58:08 +0900

    Modo-escuro

--- www/darkmode.js
+++ www/darkmode.js
@@ -1,25 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    const darkModeButton = document.querySelector('#dark-mode-button');
+    const sunIcon = document.querySelector('.fa-sun');
+    const moonIcon = document.querySelector('.fa-moon');
+    
+    // Verificar o estado do modo escuro ao carregar a página
+    const isDarkModeEnabled = localStorage.getItem('dark-mode-enabled') === 'true';
+    
+    // Definir o modo claro como padrão se não houver preferência anterior
+    if (isDarkModeEnabled) {
+        body.classList.add('dark-mode');
+        moonIcon.style.display = 'block';
+        sunIcon.style.display = 'none';
+    } else {
+        sunIcon.style.display = 'block';
+        moonIcon.style.display = 'none';
+    }
+    
+    // Função para alternar entre os modos claro e escuro
     function toggleDarkMode() {
-        const body = document.body;
         body.classList.toggle('dark-mode');
         
         const isDarkModeEnabled = body.classList.contains('dark-mode');
         localStorage.setItem('dark-mode-enabled', isDarkModeEnabled);
-    }
-    
-    document.addEventListener('DOMContentLoaded', function () {
-        const body = document.body;
-        const isDarkModeEnabled = localStorage.getItem('dark-mode-enabled') === 'true';
-    
+        
+        // Alternar ícones
         if (isDarkModeEnabled) {
-            body.classList.add('dark-mode');
+            moonIcon.style.display = 'block';
+            sunIcon.style.display = 'none';
+        } else {
+            sunIcon.style.display = 'block';
+            moonIcon.style.display = 'none';
         }
-    });
-    
-    const darkModeButton = document.querySelector('#dark-mode-button');
-    if (darkModeButton) {
-        darkModeButton.addEventListener('click', toggleDarkMode);
     }
     
-    
+    // Adicionar um ouvinte de eventos ao botão
+    darkModeButton.addEventListener('click', toggleDarkMode);
 });
