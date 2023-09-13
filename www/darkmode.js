8faad4cfd8fdd06533e4ccd8088b7e70852f2e91
LucasBatista37 <lucas.batista9734@gmail.com>
2023-09-13 10:29:37 +0900

    sobre

--- www/darkmode.js
+++ www/darkmode.js
@@ -4,10 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const sunIcon = document.querySelector('.fa-sun');
     const moonIcon = document.querySelector('.fa-moon');
     
-    // Verificar o estado do modo escuro ao carregar a página
     const isDarkModeEnabled = localStorage.getItem('dark-mode-enabled') === 'true';
     
-    // Definir o modo claro como padrão se não houver preferência anterior
     if (isDarkModeEnabled) {
         body.classList.add('dark-mode');
         moonIcon.style.display = 'block';
@@ -17,14 +15,12 @@ document.addEventListener('DOMContentLoaded', function () {
         moonIcon.style.display = 'none';
     }
     
-    // Função para alternar entre os modos claro e escuro
     function toggleDarkMode() {
         body.classList.toggle('dark-mode');
         
         const isDarkModeEnabled = body.classList.contains('dark-mode');
         localStorage.setItem('dark-mode-enabled', isDarkModeEnabled);
         
-        // Alternar ícones
         if (isDarkModeEnabled) {
             moonIcon.style.display = 'block';
             sunIcon.style.display = 'none';
@@ -34,6 +30,5 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
     
-    // Adicionar um ouvinte de eventos ao botão
     darkModeButton.addEventListener('click', toggleDarkMode);
 });
